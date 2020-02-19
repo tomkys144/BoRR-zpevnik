bd037de725173a17ebf5ab8b263509719fab8351
tomkys144 <tomkys144@gmail.com>
2020-02-19 20:07:48 +0100

    fixed song list

--- list.php
+++ list.php
@@ -7,19 +7,36 @@
 </head>
 <body>
     <div>
+        <h1>
+            Rejstřík
+        </h1>
+    </div>
+    <div style="width: 30vw; left: 40vw; top: 12em; position: absolute ; text-align: left; margin: 0">
         <?php
             error_reporting(E_ALL);
             require_once __DIR__.'/vendor/autoload.php';
             $files = scandir(__DIR__.'/songs/');
-            $list = array();
             foreach ($files as $f) {
                 if ($f === '.' || $f === '..') {
                     continue;
                 }
-                $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents($f));
-                array_push($list, '<a href="songs/' . $f . '">' . $object->matter('title') . ' - ' . $object->matter('author') . '</a>');
+                $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__.'/songs/' . $f));
+                echo('<a href="songs/' . $f . '">' . $object->matter('title') . '</a>');
             }
-            print_r($list);
+        ?>
+    </div>
+    <div style="width: 30vw; right: 40vw; top: 12em; position: absolute ; text-align: right; margin: 0">
+        <?php
+        error_reporting(E_ALL);
+        require_once __DIR__.'/vendor/autoload.php';
+        $files = scandir(__DIR__.'/songs/');
+        foreach ($files as $f) {
+            if ($f === '.' || $f === '..') {
+                continue;
+            }
+            $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__.'/songs/' . $f));
+            echo('<a href="songs/' . $f . '">' . $object->matter('author') . '</a>');
+        }
         ?>
     </div>
     <div>
