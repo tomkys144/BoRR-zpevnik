2bf3a6e099866ea4612222ec49ae3afbd24c73a3
tomkys144 <tomkys144@gmail.com>
2020-02-21 18:41:07 +0100

    fixed wrong echo order in songs.php

--- songs.php
+++ songs.php
@@ -7,42 +7,10 @@
                 header("Location: list.php");
                 exit;
             }
-            elseif ($song == count($files)-1) {
-                $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__.'/songs/' . $files[$song]));
-                echo('<div style="position: absolute; width: 64vw; left: 18vw;top: 0"><h1>' . $object->matter('title') . '</h1>');
-                echo('<h2>' . $object->matter('author') . '</h2></div>');
-                $previous_song = $song - 1;
-                if ($object->matter('capo') != null){
-                    echo('<div class="capo">Capo ' . $object->matter('capo') . '</div>');
-                }
-                echo(
-                    '<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>BoRR zpěvník - ' . $object->matter('title') . '</title>
-                        <link rel="icon" href="data/borr.png">
-                        <link rel="stylesheet" href="css.css">
-                    </head>
-                    <body>
-                    <div style="position: absolute; top: 0">
-                        <a href="index.html"><button class="icon_home"></button></a>
-                        <a href="list.php"><button class="icon_list"></button></a>
-                        <a href="help.html"><button class="icon_help"></button></a>
-                    </div>
-                    <div class="song_body"><p id="song_text">' . $object->body() . '</p></div>
-                    <div><form method="get"><input type="hidden" name="number" value="' . $previous_song . '"><button id="left_button" type="submit">&lt;</button></form></div>'
-                );
-            }
             else {
                 $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__.'/songs/' . $files[$song]));
-                echo('<div style="position: absolute; width: 64vw; left: 18vw;top: 0"><h1>' . $object->matter('title') . '</h1>');
-                echo('<h2>' . $object->matter('author') . '</h2></div>');
-                $previous_song = $song - 1;
                 $next_song = $song + 1;
-                if ($object->matter('capo') != null){
-                    echo('<div class="capo">Capo ' . $object->matter('capo') . '</div>');
-                }
+                $previous_song = $song - 1;
                 echo(
                     '<!DOCTYPE html>
                     <html lang="en">
@@ -58,10 +26,22 @@
                         <a href="list.php"><button class="icon_list"></button></a>
                         <a href="help.html"><button class="icon_help"></button></a>
                     </div>
-                    <div class="song_body"><p id="song_text">' . $object->body() . '</p></div>
-                    <div><form method="get"><input type="hidden" name="number" value="' . $next_song . '"><button id="right_button" type="submit">&gt;</button></form></div>
-                    <div><form method="get"><input type="hidden" name="number" value="' . $previous_song . '"><button id="left_button" type="submit">&lt;</button></form></div>'
+                    <div style="position: absolute; width: 64vw; left: 18vw;top: 0"><h1>' . $object->matter('title') . '</h1>
+                    <h2>' . $object->matter('author') . '</h2></div>'
                 );
+                if ($object->matter('capo') != null){
+                    echo('<div class="capo">Capo ' . $object->matter('capo') . '</div>');
+                }
+                echo ('<div class="song_body"><p id="song_text">' . $object->body() . '</p></div>');
+                if ($song == count($files) - 1) {
+                    echo ('<div><form method="get"><input type="hidden" name="number" value="' . $previous_song . '"><button id="left_button" type="submit">&lt;</button></form></div>');
+                }
+                else {
+                    echo (
+                            '<div><form method="get"><input type="hidden" name="number" value="' . $previous_song . '"><button id="left_button" type="submit">&lt;</button></form></div>
+                            <div><form method="get"><input type="hidden" name="number" value="' . $next_song . '"><button id="right_button" type="submit">&gt;</button></form></div>'
+                    );
+                }
             }
         }
         ?>
