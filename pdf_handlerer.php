2386c7481cf04b1f764ec9153c422df1d39482c5
tomkys144 <tomkys144@gmail.com>
2020-04-04 18:33:07 +0200

    Chords are being generated (broken)

--- pdf_handlerer.php
+++ pdf_handlerer.php
@@ -14,15 +14,33 @@ function str_lreplace($search, $replace, $subject)
     return $subject;
 }
 
+function line_contains($search, $needle)
+{
+    if (strpos($search, $needle) !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function verse_contains_chord($search)
+{
+    $lineEnd = strpos($search, '<br />');
+    if (line_contains(substr($search, 0, $lineEnd), '<chord>') !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function createPdfSong($songName)
 {
     $name = str_replace(' ', '_', $songName);
     $name = iconv('utf-8', 'ascii//TRANSLIT', $name);
     $name = str_replace(str_split("\:'*?<>.,!"), "", $name);
     $name = strtolower($name);
-    $name .= ".md";
 
-    $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__ . '/data/songs/' . $name));
+    $object = \Spatie\YamlFrontMatter\YamlFrontMatter::parse(file_get_contents(__DIR__ . '/data/songs/' . $name . '.md'));
 
     $pdf = new TCPDF('L', 'mm', 'A4', true, 'UTF-8', false, false);
 
@@ -43,11 +61,12 @@ function createPdfSong($songName)
     $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
     $OpenSans = TCPDF_FONTS::addTTFfont(__DIR__ . '/data/font/OpenSans/OpenSans-Regular.ttf', 'TrueTypeUnicode', '', 32);
-    $pdf->SetFont($OpenSans);
+    $pdf->SetFont($OpenSans, '', 12);
+    $OpenSansBold = TCPDF_FONTS::addTTFfont(__DIR__ . '/data/font/OpenSans/OpenSans-Bold.ttf', 'TrueTypeUnicode', '', 32);
 
     $song = $object->body();
-    $song = str_replace("<wrapper><chord>", '', $song);
-    $song = str_replace('</chord></wrapper>', '', $song);
+    $song = str_replace("<wrapper><chord>", '<chord><b>', $song);
+    $song = str_replace('</chord></wrapper>', '</b></chord>', $song);
     $song = str_replace("<br>" . PHP_EOL . "<br>", '<br>', $song);
     $song = str_replace('&#x1d106;', '||:', $song);
     $song = str_replace('𝄆', '||:', $song);
@@ -70,7 +89,7 @@ function createPdfSong($songName)
         $verseOffset = 0;
 
         //From first to last but one verses
-        for ($x=1; $x < $numberVerses; $x++) {
+        for ($n=1; $n < $numberVerses; $n++) {
             $verseStart = strpos($song, '<verse number="', $verseOffset);
             $verseEnd = strpos($song, '<verse number="', 15 + $verseStart);
             $verseLength = $verseEnd - $verseStart -1;
@@ -82,7 +101,7 @@ function createPdfSong($songName)
             $verseNoLength = $verseNoEnd - 15;
             $verseNo = substr($verse, 15, $verseNoLength);
 
-            $verseNos[]=$verseNo;
+            $verseNos[]='<b>' . $verseNo . '</b>';
         }
         //Last verse
         $verseStart = strpos($song, '<verse', $verseOffset);
@@ -92,12 +111,12 @@ function createPdfSong($songName)
         $verseNoLength = $verseNoEnd - 15;
         $verseNo = substr($verse, 15, $verseNoLength);
 
-        $verseNos[] = $verseNo;
+        $verseNos[] = '<b>' . $verseNo . '</b>';
 
         $verseOffset = 0;
 
         //From first to last but one verses
-        for ($x=1; $x < $numberVerses; $x++) {
+        for ($n=1; $n < $numberVerses; $n++) {
             $verseStart = strpos($song, '</verse>', $verseOffset)+8;
             $verseEnd = strpos($song, '<verse', $verseStart);
             $verseLength = $verseEnd - $verseStart;
@@ -125,21 +144,52 @@ function createPdfSong($songName)
         $pdf->setColumnsArray([['w'=>80, 's'=>2, 'y'=>0], ['w'=>137, 's'=>2, 'y'=>0], ['w'=>80, 's'=>2, 'y'=>0]]);
         $pdf->selectColumn($pdf->getColumn() + 1);
         $pdf->WriteHTML($songHeader, false, false, true, false, 'C');
+        if (verse_contains_chord($songText[0]) !== false) {
+            $y = $pdf->GetY()+8;
+        } else {
+            $y = $pdf->GetY()+3;
+        }
         $pdf->selectColumn($pdf->getColumn() - 1);
-        $pdf->SetY(40, false, true);
-        for ($x=0; $x < $numberVerses; $x++) {
-            $pdf->WriteHTML($verseNos[$x], false, false, true, false, 'R');
+        $pdf->SetY($y, false, true);
+        for ($n=0; $n < $numberVerses; $n++) {
+            $pdf->WriteHTML($verseNos[$n], false, false, true, false, 'R');
             $y = $pdf->GetY();
             $pdf->selectColumn($pdf->getColumn() + 1);
             $pdf->SetY($y, false, true);
-            $pdf->WriteHTML($songText[$x], false, false, true, false, 'L');
-            $y = $pdf->GetY()+2;
+
+            $chordOffset = 0;
+            for ($m=1; $m <= substr_count($songText[$n], '<chord>'); $m++) {
+                $chordStart = strpos($songText[$n], '<chord>', $chordOffset);
+                $chordEnd = strpos($songText[$n], '</chord>', $chordStart);
+                if (line_contains(substr($songText[$n], $chordOffset, $chordStart-$chordOffset), '<br />')) {
+                    $break = strpos($songText[$n], '<br />', $chordOffset)+6;
+                    $pdf->WriteHTML(substr($songText[$n], $chordOffset, $break-$chordOffset), false, false, true, false, 'L');
+                    $pdf->SetY($pdf->GetY()+5, false, true);
+                    $pdf->WriteHTML(substr($songText[$n], $break, $chordStart-$break), false, false, true, false, 'L');
+                } else {
+                    $pdf->WriteHTML(substr($songText[$n], $chordOffset, $chordStart - $chordOffset), false, false, true, false, 'L');
+                }
+                $x = $pdf->GetX();
+                $pdf->SetXY($x, $pdf->GetY()-4.7,  true);
+                $pdf->WriteHTML(substr($songText[$n], $chordStart+7, $chordEnd-$chordStart-7), false, false, true, false, 'L');
+                $pdf->SetXY($x, $pdf->GetY()+4.7, true);
+                $chordOffset=$chordEnd+8;
+            }
+            $pdf->WriteHTML(substr($songText[$n], $chordOffset), false, false, true, false, 'L');
+
+            if ($n !== $numberVerses-1) {
+                if (verse_contains_chord($songText[$n+1]) !== false) {
+                    $y = $pdf->GetY() + 8;
+                } else {
+                    $y = $pdf->GetY() + 3;
+                }
+            }
             $pdf->selectColumn($pdf->getColumn() - 1);
             $pdf->SetY($y, false, true);
         }
     }
-    $pdf->Output('test.pdf', 'I');
+    $pdf->Output($name . '.pdf', 'I');
 
 }
 
-createPdfSong('Test');
+createPdfSong('Blemst');
